2be96fca006045e89a4ab8601836c75688381138
Abdullah Zaydi <76549765+AbdullahZaydi@users.noreply.github.com>
2024-08-02 21:11:26 +0500

    Websocket build issues

--- src/prover/websocket_client.cpp
+++ src/prover/websocket_client.cpp
@@ -1,23 +1,26 @@
 #include "websocket_client.hpp"
 #include <iostream>
 #include <thread>
-#include <functional> // Include this to use std::bind and std::placeholders
+#include <functional>
 
-WebSocketClient::WebSocketClient() : m_open(false), m_done(false) {
+WebSocketClient::WebSocketClient() : m_open(false), m_done(false)
+{
     m_client.init_asio();
     m_client.set_open_handler(std::bind(&WebSocketClient::on_open, this, std::placeholders::_1));
     m_client.set_message_handler(std::bind(&WebSocketClient::on_message, this, std::placeholders::_1, std::placeholders::_2));
     m_client.set_close_handler(std::bind(&WebSocketClient::on_close, this, std::placeholders::_1));
 }
 
-void WebSocketClient::on_open(websocketpp::connection_hdl hdl) {
+void WebSocketClient::on_open(websocketpp::connection_hdl hdl)
+{
     m_hdl = hdl;
     m_open = true;
     m_cond.notify_all();
     std::cout << "Connection opened" << std::endl;
 }
 
-void WebSocketClient::on_message(websocketpp::connection_hdl hdl, client::message_ptr msg) {
+void WebSocketClient::on_message(websocketpp::connection_hdl hdl, client::message_ptr msg)
+{
     {
         std::lock_guard<std::mutex> lock(m_mutex);
         m_response = msg->get_payload();
@@ -27,31 +30,39 @@ void WebSocketClient::on_message(websocketpp::connection_hdl hdl, client::messag
     std::cout << "Received message: " << m_response << std::endl;
 }
 
-void WebSocketClient::on_close(websocketpp::connection_hdl hdl) {
+void WebSocketClient::on_close(websocketpp::connection_hdl hdl)
+{
     std::cout << "Connection closed" << std::endl;
 }
 
-void WebSocketClient::run(const std::string &uri) {
+void WebSocketClient::run(const std::string &uri)
+{
     websocketpp::lib::error_code ec;
     client::connection_ptr con = m_client.get_connection(uri, ec);
-    if (ec) {
+    if (ec)
+    {
         std::cout << "Could not create connection because: " << ec.message() << std::endl;
         return;
     }
 
     m_client.connect(con);
-    std::thread([this]() { m_client.run(); }).detach();
+    std::thread([this]()
+                { m_client.run(); })
+        .detach();
 }
 
-std::string WebSocketClient::send_and_receive(const std::string &message) {
+std::string WebSocketClient::send_and_receive(const std::string &message)
+{
     // Wait for the connection to open
     std::unique_lock<std::mutex> lock(m_mutex);
-    m_cond.wait(lock, [this] { return m_open; });
+    m_cond.wait(lock, [this]
+                { return m_open; });
 
     m_client.send(m_hdl, message, websocketpp::frame::opcode::text);
 
     // Wait for the response
-    m_cond.wait(lock, [this] { return m_done; });
+    m_cond.wait(lock, [this]
+                { return m_done; });
 
     return m_response;
 }
--- src/prover/websocket_client.hpp
+++ src/prover/websocket_client.hpp
@@ -1,11 +1,11 @@
-#ifndef WEBSOCKETCLIENT_HPP
-#define WEBSOCKETCLIENT_HPP
+#ifndef WEBSOCKET_CLIENT_HPP
+#define WEBSOCKET_CLIENT_HPP
 
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 #include <string>
-#include <condition_variable>
 #include <mutex>
+#include <condition_variable>
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
@@ -13,7 +13,6 @@ class WebSocketClient
 {
 public:
     WebSocketClient();
-
     void run(const std::string &uri);
     std::string send_and_receive(const std::string &message);
 
@@ -24,11 +23,11 @@ private:
 
     client m_client;
     websocketpp::connection_hdl m_hdl;
-    std::string m_response;
     std::mutex m_mutex;
     std::condition_variable m_cond;
+    std::string m_response;
     bool m_open;
     bool m_done;
 };
 
-#endif
+#endif // WEBSOCKET_CLIENT_HPP
